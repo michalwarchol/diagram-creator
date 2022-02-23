6cb41860406a4140e22b57cd1713f2e68be00ecc
Michał Warchoł <imsimilar@gmail.com>
2022-02-23 20:12:54 +0100

    :hammer: Create addElement and moveElement cases

--- redux/reducer.ts
+++ redux/reducer.ts
@@ -1,25 +1,58 @@
-import {Selector, State, Actions} from "./types";
+import { Selector, State, Actions } from "./types";
 
 const initialState: State = {
-    selector: Selector.SHAPES,
-    activeElements: [],
-}
+  selector: Selector.SHAPES,
+  activeElements: [],
+  elements: [],
+  elementsCounter: 0,
+};
 
 const reducer = (state: State = initialState, action: Actions) => {
-    switch(action.type){
-        case "changeSelector":
-            return {
-                ...state,
-                selector: action.selector
-            };
-        case "changeActiveElements":
-            return {
-                ...state,
-                activeElements: [...action.activeElements]
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case "changeSelector":
+      return {
+        ...state,
+        selector: action.selector,
+      };
+    case "changeActiveElements":
+      return {
+        ...state,
+        activeElements: [...action.activeElements],
+      };
+    case "addElement":
+      const { id, C, height, width, backgroundColor, x, y } = action.element;
+      return {
+        ...state,
+        elements: [
+          ...state.elements,
+          {
+            id,
+            C,
+            height: height || 100,
+            width: width || 100,
+            backgroundColor,
+            x: x || 100,
+            y: y || 100,
+          },
+        ],
+        elementsCounter: state.elementsCounter + 1,
+      };
+
+    case "moveElement":
+        return {
+            ...state,
+            elements: state.elements.map(elem=>{
+                if(elem.id != action.id) return elem;
+                return {
+                    ...elem,
+                    x: action.x,
+                    y: action.y
+                }
+            })
+        }
+    default:
+      return state;
+  }
+};
 
 export default reducer;
