dba3dcd62a7ac377f03c79e9000a5b2d4995aae9
Michał Warchoł <imsimilar@gmail.com>
2022-02-23 20:14:05 +0100

    :ab: Create elementProperties, BaseElementProperities and ElementUnitProps types

--- redux/types.ts
+++ redux/types.ts
@@ -1,25 +1,47 @@
+import React from "react";
+
 export enum Selector {
-    SHAPES,
-    TEXT,
-    ELEMENTS,
-    ARROWS
+  SHAPES,
+  TEXT,
+  ELEMENTS,
+  ARROWS,
 }
 
-export type State = {
-    selector: Selector;
-    activeElements: HTMLElement[];
-}
-
-export type Actions = 
-    | {type: "changeSelector"; selector: Selector}
-    | {type: "changeActiveElements"; activeElements: HTMLElement[]}
-
-
-
-export type BaseElementProperties = {
+export type ElementProperties = {
+    id: number;
+    C: React.FC<ElementUnitProps>;
     height: number;
     width: number;
     backgroundColor: string;
     x: number;
     y: number;
-}
+  };
+
+export type BaseElementProperties = {
+  id: number;
+  C: React.FC<ElementUnitProps>;
+  height?: number;
+  width?: number;
+  backgroundColor?: string;
+  x?: number;
+  y?: number;
+};
+
+export type ElementUnitProps = {
+  h: number; //element height
+  w: number; //element width
+  color: string;
+};
+
+export type State = {
+  selector: Selector;
+  activeElements: HTMLElement[];
+  elements: ElementProperties[];
+  elementsCounter: number;
+};
+
+export type Actions =
+  | { type: "changeSelector"; selector: Selector }
+  | { type: "changeActiveElements"; activeElements: HTMLElement[] }
+  | { type: "addElement"; element: BaseElementProperties }
+  | { type: "moveElement"; id: number; x: number; y: number };
